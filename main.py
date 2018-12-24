e960f0d9672131eaf59ac19b364b9c4b4c337226
Eoin Wickens <eoin.wickens@mycit.ie>
2018-12-24 13:06:16 +0000

    Finished guide with 64% accuracy

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -119,12 +120,12 @@ def colour_to_gray(images28):
     return grayImages28
 
 
-def tensor_init():
+def tensor_init(images28):
     # A tensor in this case is basically an image
 
     # Initialize placeholders AKA Uninitialized variables
     x = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28])
-    y = tf.placeholder(dtype=tf.int32, shape=[None]) # True array
+    y = tf.placeholder(dtype=tf.int32, shape=[None])  # True array
 
     # Flatten the input data into an array of type [None, 784] instead of [None,28,28]
     # From my understanding it takes a three dimensional array and makes it into a two
@@ -158,6 +159,63 @@ def tensor_init():
     print("predicted_labels: ", correct_pred)
     print("accuracy: ", accuracy)
 
+    tf.set_random_seed(1234)
+
+    sess = tf.Session()
+
+    sess.run(tf.global_variables_initializer())
+
+    for i in range(201):  # According to guide 201 is chosen as you want to be able to register the last loss value?
+        print('EPOCH', i)  # An Epoch is one full training cycle on the training set
+        _, accuracy_val = sess.run([train_op, accuracy],
+                                   feed_dict={x: images28, y: labels})  # Feeds the data into the neural net
+        if i % 10 == 0:
+            print("Loss: ", loss)
+        print('DONE WITH EPOCH')
+
+        # Pick 10 random images
+    sample_indexes = random.sample(range(len(images28)), 10)
+    sample_images = [images28[i] for i in sample_indexes]
+    sample_labels = [labels[i] for i in sample_indexes]
+
+    # Run the "correct_pred" operation
+    predicted = sess.run([correct_pred], feed_dict={x: sample_images})[0]
+
+    # Print the real and predicted labels
+    print(sample_labels)
+    print(predicted)
+
+    # Display the predictions and the ground truth visually.
+    fig = plt.figure(figsize=(10, 10))
+    for i in range(len(sample_images)):
+        truth = sample_labels[i]
+        prediction = predicted[i]
+        plt.subplot(5, 2, 1 + i)
+        plt.axis('off')
+        color = 'green' if truth == prediction else 'red'
+        plt.text(40, 10, "Truth:        {0}\nPrediction: {1}".format(truth, prediction),
+                 fontsize=12, color=color)
+        plt.imshow(sample_images[i], cmap="gray")
+
+    plt.show()
+
+    testImages, testLabels = load_data(test_data_directory)
+    testImages28 = resize_images(testImages)
+    grayTestImages28 = colour_to_gray(testImages28)
+
+    # Run predictions against the full test set.
+    predicted = sess.run([correct_pred], feed_dict={x: grayTestImages28})[0]
+
+    # Calculate correct matches
+    match_count = sum([int(y == y_) for y, y_ in zip(testLabels, predicted)])
+
+    # Calculate the accuracy
+    accuracy = match_count / len(testLabels)
+
+    # Print the accuracy
+    print("Accuracy: {:.3f}".format(accuracy))
+
+
 ROOT_PATH = "C:\\Users\\Eoinw\\Dropbox\\College\\Year 3\\Programming for Data Analytics\\Labs\\Tensor\\BelgiumSigns"
 train_data_directory = os.path.join(ROOT_PATH, "BelgiumTSC_Training/Training")
 test_data_directory = os.path.join(ROOT_PATH, "BelgiumTSC_Testing/Testing")
@@ -169,6 +227,10 @@ images, labels = load_data(train_data_directory)
 images28 = resize_images(images)
 grayImages28 = colour_to_gray(images28)
 
-plot_random_images(grayImages28)
+# plot_random_images(grayImages28)
 
-tensor_init()
+
+
+tensor_init(grayImages28)
+
+
